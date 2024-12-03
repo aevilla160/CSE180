e0db6316a159cd7bc434a33868ce81baea55310e
killgorealpha <killgorealpha@protonmail.com>
2024-12-03 13:59:05 -0800

    modified:   client/Actor.tscn 	modified:   client/inventory/InventoryUISlot.tscn 	modified:   client/inventory/inventory_ui.gd 	new file:   client/inventory/inventory_ui_slot.gd 	modified:   client/ui/UI.tscn

--- client/inventory/inventory_ui.gd
+++ client/inventory/inventory_ui.gd
@@ -1,9 +1,18 @@
 extends Control
 
+@onready var inventory: Inventory = preload("res://inventory/player_inventory.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var is_open = false
 
 func _ready() -> void:
+	update_slots()
 	close()
+
+
+func update_slots():
+	for i in range(min(inventory.items.size(), slots.size())):
+		slots[i].update(inventory.items[i])
 
 
 func _process(delta: float) -> void:
