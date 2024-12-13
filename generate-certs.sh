b1b67336a6f1624dabb4a84afb183693794b4c0a
killgorealpha <killgorealpha@protonmail.com>
2024-12-13 11:37:20 -0800

    modified:   generate-certs.sh

--- generate-certs.sh
+++ generate-certs.sh
@@ -5,20 +5,20 @@ mkdir -p server/certs
 mkdir -p /certs
 cd certs
 rm *.key
-rm *.cert
+rm *.crt
 rm *.req
 
 # 1. Generate CA's private key and self-signed certificate
 openssl req -x509 \
     -newkey rsa:4096 \
     -keyout ca-key.key \
-    -out ca-cert.cert \
+    -out ca-cert.crt \
     -days 365 \
     -nodes \
     -subj "/C=US/ST=California/CN=149.28.223.185"
 
 echo "CA's self-signed certificate"
-openssl x509 -in ca-cert.cert -noout -text
+openssl x509 -in ca-cert.crt -noout -text
 
 # 2. Generate web server's private key and certificate signing request (CSR)
 openssl req \
@@ -32,16 +32,16 @@ openssl req \
 openssl x509 -req \
     -in server-req.req \
     -days 60 \
-    -CA ca-cert.cert \
+    -CA ca-cert.crt \
     -CAkey ca-key.key \
     -CAcreateserial \
-    -out server-cert.cert \
+    -out server-cert.crt \
     -extfile server-ext.cnf
 
 echo "Server's signed certificate"
-openssl x509 -in server-cert.cert -noout -text
+openssl x509 -in server-cert.crt -noout -text
 
-openssl verify -CAfile ca-cert.cert server-cert.cert
+openssl verify -CAfile ca-cert.crt server-cert.crt
 
 cp *.key ../server/certs/
-cp *.cert ../server/certs/
+cp *.crt ../server/certs/
