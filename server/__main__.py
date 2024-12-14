46840a1646da2f8f4e4a11b087367beea679ec8f
killgorealpha <killgorealpha@protonmail.com>
2024-12-14 14:17:02 -0800

    modified:   client/project.godot 	modified:   client/websockets_client.gd 	modified:   server/__main__.py

--- server/__main__.py
+++ server/__main__.py
@@ -60,8 +60,8 @@ if __name__ == '__main__':
 
     certs_dir: str = f"{sys.path[0]}/certs/"
 
-    # private_key_data = open(os.path.join(certs_dir, "ca-key.key"), "rb").read()
-    # certificate_data = open(os.path.join(certs_dir, "ca-cert.crt"), "rb").read()
+    # private_key_data = open(os.path.join(certs_dir, "server.key"), "rb").read()
+    # certificate_data = open(os.path.join(certs_dir, "server.crt"), "rb").read()
 
     # private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key_data)
     # certificate = crypto.load_certificate(crypto.FILETYPE_PEM, certificate_data)
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     HOSTNAME = 'gameserver.killgorealpha.com'
     factory = GameFactory(HOSTNAME, PORT)
     # factory = GameFactory('0.0.0.0', PORT)
-    contextFactory = ssl.DefaultOpenSSLContextFactory(certs_dir + "ca-key.key", certs_dir + "ca-cert.crt")
+    contextFactory = ssl.DefaultOpenSSLContextFactory(certs_dir + "server.key", certs_dir + "server.crt")
 
     logger.info(f"Server listening on port {PORT}")
     reactor.listenSSL(PORT, factory, contextFactory)
