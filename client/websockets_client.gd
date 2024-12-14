98fdf098ce9ca18520dfe648ba674ed5cacb96f0
killgorealpha <killgorealpha@protonmail.com>
2024-12-13 16:40:32 -0800

    modified:   client/websockets_client.gd 	modified:   server/__main__.py

--- client/websockets_client.gd
+++ client/websockets_client.gd
@@ -16,7 +16,7 @@ var tls_cert := X509Certificate.new()
 #var reconnect_timer = 0.0
 
 func _ready():
-	var hostname = "149.28.223.185"
+	var hostname = "killgorealpha.com"
 	var port = 8081
 	var websocket_url = "wss://%s:%d" % [hostname, port]
 	
