36dabd2cc7dac9e0df6a31d54994b914c0a82910
Alex Villa <aevilla160@gmail.com>
2024-12-17 18:05:02 -0800

    Final map edits

--- client/Chatbox.gd
+++ client/Chatbox.gd
@@ -24,7 +24,76 @@ func _input(event: InputEvent):
 
 func add_message(username, text: String):
 	if username:
-		chat_log.text += username + ' says: "' + text + '"\n'
+		if text == "<3": 
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "\u2665" + "\n"		
+		elif text == ":)"	:
+			chat_log.text += "[color=green]" + username + "[/color] says: " + "😀" + "\n"
+		elif text == ";)"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😉" + "\n"
+		elif text == "B)"	:
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😎" + "\n"
+		elif text == ">:("	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😠" + "\n"
+		elif text == ":|"	:
+			chat_log.text += "[color=white]" + username + "[/color] says: " + "😐" + "\n"
+		elif text == ":("	:
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😔" + "\n"
+		elif text == ":,("	:
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😢" + "\n"
+		elif text == "(:"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "🙃" + "\n"
+		elif text == ":P"	:
+			chat_log.text += "[color=pink]" + username + "[/color] says: " + "😛" + "\n"
+		elif text == ":o"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😯" + "\n"
+		elif text == "XD"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😆" + "\n"
+		elif text == ":3"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😗" + "\n"	
+		elif text == "0_0"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😳" + "\n"	
+		elif text == "ToT"	:
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😭" + "\n"	
+		elif text == ">w<"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😫" + "\n"	
+		elif text == "0//0"	:
+			chat_log.text += "[color=red]" + username + "[/color] says: " + "😳" + "\n"
+		elif text == "Ammon":
+			chat_log.text += "[color=green]" + username + "[/color] says: " + "🐐" + "\n"
+		elif text == "Test":
+			chat_log.text += "[color=white]" + username + "[/color] says: " + text + "\n"
+			chat_log.text += "[color=purple]" + "Dev" + "[/color] says: " + "[color=purple]" +  "0/10 You Failed" + "[/color]" + "\n"
+		elif text == "Hello":
+			chat_log.text += "[color=white]" + username + "[/color] says: " + text + "\n"
+			chat_log.text += "[color=purple]" + "Dev" + "[/color] says: "  +  "Hi "+ "[color=purple]" + username + "[/color]" + "\n"
+		elif text == "-_-":
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😑" + "\n"
+		elif text == ":D":
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😃" + "\n"
+		elif text == "^_^":
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😁" + "\n"
+		elif text == "8)":
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "🤓" + "\n"
+		elif text == "Bye":
+			chat_log.text += "[color=white]" + username + "[/color] says: " + text + "\n"
+			chat_log.text += "[color=purple]" + "Dev" + "[/color] says: " + "[color=purple]" +  "See You Later" + "[/color]" + "\n"
+		elif text == ";p":
+			chat_log.text += "[color=blue]" + username + "[/color] says: " + "😜" + "\n"
+		elif text == "Alex":
+			chat_log.text += "[color=white]" + username + "[/color] says: " + text + "\n"
+			chat_log.text += "[color=purple]" + "Dev" + "[/color] says: " + "[color=purple]" +  "Parallel Computing" + "[/color]" + "\n"
+		elif text == "Armando":
+			chat_log.text += "[color=white]" + username + "[/color] says: " + text + "\n"
+			chat_log.text += "[color=purple]" + "Dev" + "[/color] says: " + "[color=purple]" +  "RIP TicTacToe" + "[/color]" + "\n"
+		elif text == "Jeffrey":
+			chat_log.text += "[color=white]" + username + "[/color] says: " + text + "\n"
+			chat_log.text += "[color=purple]" + "Dev" + "[/color] says: " + "[color=purple]" +  "Please Don't Sue Nintendo" + "[/color]" + "\n"
+		
+		
+		
+
+		else:	
+			chat_log.text += username + ' says: "' + text + '"\n'
 	else:
 		# Server message
 		chat_log.text += "[color=yellow]" + text + "[/color]\n" 
